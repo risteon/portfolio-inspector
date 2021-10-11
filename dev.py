3e9ecc1c1c05bed8a6ff281433ae571755c14f36
Christoph Rist <c.rist@posteo.de>
2021-10-11 22:10:57 +0200

    Retrieve symbols from isin

--- dev.py
+++ dev.py
@@ -3,6 +3,8 @@ import datetime
 import pathlib
 
 import click
+import numpy as np
+import pandas as pd
 import pyEX
 import requests
 
@@ -10,16 +12,30 @@ from portfolio_inspector.data_provider import DataProviderCached, DataProviderIE
 
 
 @click.command()
-def main():
+@click.argument("trades", type=click.File("r"))
+def main(trades):
 
     data_provider_iex_cloud = DataProviderIEXCloud()
     data_provider = DataProviderCached(data_provider_iex_cloud)
 
+    xx = pd.read_csv(trades)
+    xx.dropna(subset=["Date"], inplace=True)
+
+    isin_unique: np.ndarray = pd.unique(xx["ISIN"])
+    print(isin_unique)
+
+    data_provider.lookup_isin(isin_unique[:4])
+
+    quit(0)
+
     date_start = datetime.date(2021, 8, 3)
     date_end = datetime.date(2021, 8, 25)
 
+    data_provider.get_symbol_meta("AAPL")
+
     # chart = data_provider.get_chart_day_range("AAPL", date_start, date_end)
-    chart = data_provider.get_chart_day_range("GOOGL", date_start, date_end)
+    chart = data_provider.get_chart_day_range("AAPL", date_start, date_end)
+    # chart = data_provider.get_chart_day_range("GOOGL", date_start, date_end)
 
     print(chart)
 
--- portfolio_inspector/data_provider.py
+++ portfolio_inspector/data_provider.py
@@ -4,6 +4,7 @@ import datetime
 import logging
 import pathlib
 
+import numpy as np
 import pandas as pd
 import pyEX
 
@@ -26,6 +27,14 @@ class DataProviderIEXCloud:
     def __repr__(self):
         return f"iexcloud_{self._version}"
 
+    def get_symbol_meta(self, symbol: str):
+        print(symbol)
+        self._client
+        raise NotImplementedError()
+
+    def lookup_isin(self, isin: str) -> pd.DataFrame:
+        return self._client.isinLookupDF(isin)
+
     def get_chart_day_range(self, symbol, date_start: datetime.date) -> pd.DataFrame:
         chart = self._client.chartDF(
             symbol=symbol, closeOnly=True, last=7, timeframe="5y"
@@ -40,7 +49,7 @@ class DataProviderCached:
         self._data_provider = data_provider
 
         if cache_location is None:
-            self.cache_location = DataProviderCached._get_default_cache_location()
+            self.cache_location = DataProviderCached.get_default_cache_location()
         else:
             self.cache_location = cache_location
 
@@ -50,17 +59,40 @@ class DataProviderCached:
             self.cache_location.mkdir(parents=True)
 
     @staticmethod
-    def _get_default_cache_location() -> pathlib.Path:
+    def get_default_cache_location() -> pathlib.Path:
         cache_location = pathlib.Path.home() / ".cache"
         if not cache_location.is_dir():
             raise RuntimeError("User cache folder unavailable.")
         cache_location /= "portfolio-inspector"
         return cache_location
 
+    def get_symbol_meta(self, symbol: str):
+        return self._data_provider.get_symbol_meta(symbol)
+
+    def lookup_isin(self, isin: np.ndarray):
+        cache_file = self.cache_location / "isin_symbol.pd"
+
+        try:
+            mapping: pd.DataFrame = pd.read_parquet(cache_file)
+            # check which isin are already available in mapping
+            available = mapping.index.levels[0].values
+            missing = list(set(isin) - set(available))
+
+        except FileNotFoundError:
+            missing = isin
+
+        if missing:
+            isin_symbols = [self._data_provider.lookup_isin(x) for x in missing]
+            mapping_update = pd.concat(dict(zip(missing, isin_symbols)))
+            mapping = pd.concat((mapping, mapping_update))
+            mapping.to_parquet(cache_file)
+
+        return mapping
+
     def get_chart_day_range(
-        self, symbol, date_start: datetime.date, date_end: datetime.date
+        self, symbol: str, date_start: datetime.date, date_end: datetime.date
     ):
-        cache_file = self.cache_location / f"{symbol}.pd"
+        cache_file = self.cache_location / f"chart_{symbol}.pd"
         try:
             chart = pd.read_parquet(cache_file)
             logger.info(f"Loaded from cache {str(cache_file)}.")
